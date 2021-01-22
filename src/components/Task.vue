94122f24aa360cde415ae0bd5107ed9e592f4b1b
shobaseprg <pgymlsho19128@gmail.com>
2021-01-23 03:03:55 +0900

    form_get

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="task">
     <button type="button" @click="logout" id="logout">ログアウト</button>
+    <form>
+      <input type="text" v-model="message" id="message" />
+      <div @click="send">送信</div>
+    </form>
   </div>
 </template>
 
 <script>
 import firebase from "firebase"; // firebaseのインポート
 
+// const db = firebase.firestore();
+// db.settings({
+//   timestampsInSnapshots: true,
+// });
+// const collection = db.collection("messages");
+// const submitBtn = document.getElementById("submit-button");
+
+// collection.orderBy("created").onSnapshot((snapshot) => {
+//   /* messageCollectionに変化があった場合 */
+//   snapshot.docChanges().forEach((change) => {
+//     /* snapshot.docChanges()の返り値は、コレクション自体 */
+//     if (change.type === "added") {
+//       /* コレクションのデータ変化が追加出会った場合？ */
+//       chatMessages.push(change.doc.data());
+//     }
+//   });
+// });
+
+// submitBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+// const val = message.value.trim();
+// if (val === "") return;
+
+// message.value = "";
+// message.focus();
+
+// collection
+//   .add({
+//     message: val,
+// userName: loginUser.displayName,
+// created: firebase.firestore.FieldValue.serverTimestamp(),
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
+// message.focus();
+
 export default {
   name: "logout",
-  created() {
-    var user = firebase.auth().currentUser;
-    console.log(user);
+  data: function () {
+    return {
+      message: "",
+    };
   },
   methods: {
     logout: function () {
@@ -22,6 +67,9 @@ export default {
           console.log("ログアウトしました");
           location.href = "/";
         });
+    },
+    send: function () {
+      console.log(this.message);
     },
   },
 };
